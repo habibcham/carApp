455a101dc7fd46335bf139a4de2bde760d60f97e
habibcham <34128032+habibcham@users.noreply.github.com>
2018-02-10 16:36:52 +0000

    Add files via upload

---
+++ main.js
@@ -0,0 +1,21 @@
+import "./main.css";
+
+// Import libraries we need.
+import { default as Web3} from 'web3';
+import { default as contract } from 'truffle-contract'
+import { default as BigNumber } from 'bignumber.js'
+import { Template } from 'meteor/templating';
+import { ReactiveVar } from 'meteor/reactive-var';
+
+import './index.html';
+import './index1.html';
+import './index2.html';
+import './index3.html';
+import './index4.html';
+import './index5.html';
+import './index6.html';
+import './index7.html';
+
+var accounts;
+var account;
+var account1;
